19a65db32365ace15de6485c95b29daa5cc323a0
ahtsho <tsegaye.ahadu@gmail.com>
2015-05-12 10:22:31 +0200

    Class to manage console view

---
+++ labyrinth/src/view/Console.java
@@ -0,0 +1,18 @@
+package view;
+
+public class Console {
+	
+	public static String HORIZONTAL_WALL = "=====";
+	public static String NO_HORIZONTAL_WALL = "-.-.-";
+	public static String HORIZONTAL_SPACE = "-----";
+	
+	public static String CORNER = "+";
+	public static String NO_CORNER = "*";
+	
+	public static String VERTICAL_WALL = "|";
+	public static String NO_VERTICAL_WALL = "x";	
+	
+	
+	
+	
+}
